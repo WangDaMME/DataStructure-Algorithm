340d477f55ab002fdc88a419c7f3d91eae8ccf3c
Wang Da - Mech <48291391+WangDaMME@users.noreply.github.com>
2020-03-18 08:46:45 -0400

    Add files via upload

---
+++ Tree/BinaryTre.py
@@ -0,0 +1,25 @@
+class BinaryTreeNode(object):
+    def __init__(self,data,Lchild=None,Rchild=None):
+        self.data=data
+        self.lchild=Lchild
+        self.rchild = Rchild
+
+
+a=BinaryTreeNode("A")
+b=BinaryTreeNode("B")
+c=BinaryTreeNode("C")
+d=BinaryTreeNode("D")
+e=BinaryTreeNode("E")
+f=BinaryTreeNode("F")
+g=BinaryTreeNode("G")
+
+root=e
+e.lchild=a
+e.rchild=g
+g.rchild=f
+a.rchild=c
+c.lchild=b
+c.rchild=d
+
+print(root.lchild.rchild.lchild.data)
+print(root.rchild.lchild)
