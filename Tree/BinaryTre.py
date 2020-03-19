1a0732f4610fc4e4456029fbdfb34c682c5bba9f
Wang Da - Mech <48291391+WangDaMME@users.noreply.github.com>
2020-03-19 15:57:12 -0400

    Add: Transverse 4 types: pre/middle/post +layer

--- Tree/BinaryTre.py
+++ Tree/BinaryTre.py
@@ -1,5 +1,3 @@
-# Refer to BinaryTree.png
-
 class BinaryTreeNode(object):
     def __init__(self,data,Lchild=None,Rchild=None):
         self.data=data
@@ -23,5 +21,56 @@ a.rchild=c
 c.lchild=b
 c.rchild=d
 
+'''
 print(root.lchild.rchild.lchild.data)
 print(root.rchild.lchild)
+'''
+
+# 4 种遍历
+#1. 前序遍历 [根 左 右]
+#2. 中序遍历 [左 根 右]
+#3. 右序遍历 [左 右 根]
+#4. 层次遍历  每层遍历 需要用到 队列
+
+def pre_order(root):
+    if root:  #如果右节点  再遍历
+        print(root.data,end=',')
+        pre_order(root.lchild)
+        pre_order(root.rchild)
+
+def middle_order(root):
+    if root:
+        middle_order(root.lchild)
+        print(root.data,end=',')
+        middle_order(root.rchild)
+
+def post_order(root):
+    if root:
+        post_order(root.lchild)
+        post_order(root.rchild)
+        print(root.data,end=',')
+
+#4 层次遍历
+from collections import deque
+
+def layer_order(root):
+    queue=deque()
+    queue.append(root)
+    while (len(queue)>0):  # 队列不为空 常用语句
+        node=queue.popleft()
+        print(node.data,end=',')
+        if node.lchild:  # 如果有 左孩子节点
+            queue.append(node.lchild)
+        if node.rchild:  # 如果有 右孩子节点
+            queue.append(node.rchild)
+
+
+pre_order(e)  #E,A,C,B,D,G,F,
+print('\n')
+middle_order(e) #A,B,C,D,E,G,F,
+print('\n')
+post_order(e) #B,D,C,A,F,G,E,
+print('\n')
+print('层次')
+layer_order(e)
+
