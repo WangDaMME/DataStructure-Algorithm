e533047aaf86e51cd790ac454162c0572fa29868
Wang Da - Mech <48291391+WangDaMME@users.noreply.github.com>
2020-03-25 12:41:52 -0400

    Add files via upload

--- Tree/BinarySearchTree.py
+++ Tree/BinarySearchTree.py
@@ -105,13 +105,88 @@ class BSTree(object):
                 return p
         else:
             return None # 执行完 找不到
-                    
 
+#=============== Delete ==============#
+    #case1: 删除叶子节点
+    def __remove__node_1(self,node):
+        # 先判断是不是根节点
+        if not node.parent:   # parent为空的话
+            self.root=None
+        if node is node.parent.lchild:
+            node.parent.lchild=None
+            # node.parent=None  #写不写都行， 因为码从上往下执行 ，已经找不到 这个node了
+        else:   #node 是 rchild 节点
+            node.parent.rchild=None
+            #node.parent=None
 
+    #case2: 只有一个节点   --[左]
+    # 2.1 只有一个左孩子节点
+    def __remove__node_2L(self,node):
+        # 先判断是不是根节点
+        if not node.parent:
+            #更新根
+            self.root=node.lchild
+            node.lchild.parent=None  # 新根父 节点为 None
 
+        # 该节点 为 左孩子节点
+        elif node is node.parent.lchild:
+            node.parent.lchild=node.lchild
+            node.lchild.parent=node.parent
+        else:  # 该节点为 右孩子节点
+            node.parent.rchild=node.lchild
+            node.lchild.parent=node.parent
+
+    # case2: 只有一个节点
+    # 2.2 只有一个右孩子节点
+    def __remove__node_2R(self, node):
+        # 先判断是不是根节点
+        if not node.parent:
+            # 更新根
+            self.root = node.rchild
+            node.rchild.parent = None  # 新根父 节点为 None
+        # 该节点 为 左孩子节点
+        elif node is node.parent.lchild:
+            node.parent.lchild = node.rchild
+            node.rchild.parent = node.parent
+        else:  # 该节点为 右孩子节点
+            node.parent.rchild = node.rchild
+            node.rchild.parent = node.parent
+
+    # case3: 包含对 case1,2调用    #两个节点
+    def delete(self,value):
+        # 不是空树
+        if self.root:
+            node=self.query_no_recursion(value)
+            if not node:
+                raise Exception('This node does not exist!')
+
+            #case1: 叶子节点
+            # 是叶子节点 --- 没有其他自己节点
+            if not node.lchild and not node.rchild: #两个都没有
+                self.__remove__node_1(node)
+            #case2: 只有一个节点
+            # 2.1 只有一个【左】孩子节点
+            elif not node.rchild:  # 没有右孩子
+                self.__remove__node_2L(node)
+            elif not node.lchild:  # 没有左孩子
+                self.__remove__node_2R(node)
+            else:  #三个节点 都没有
+                min_node=node.rchild  # 将其右子树 最小节点 拿上来 删除
+                while min_node.lchild:  # 最小一路往左看
+                    min_node=min_node.lchild
+                #不删了，把 值 替换一下好了
+                node.data=min_node.data
+                # 删除 最小节点
+                # 只有一个右节点
+                if min_node.rchild:  # 不可能 再有 左节点了 要不更小
+                    self.__remove__node_2R(min_node)
+                else:  #没有子节点
+                    self.__remove__node_1(min_node)
 
 
 import random
+'''
+
 lst=list(range(10))
 random.shuffle(lst)
 tree=BSTree(lst)
@@ -131,3 +206,23 @@ random.shuffle(lst2)
 tree2=BSTree(lst2)
 print(tree2.query_no_recursion(4).data)
 print(tree2.query_no_recursion(3))
+'''
+print('======== Delete Func Test ==========')
+
+lst=list(range(10))
+random.shuffle(lst)
+print(lst)
+
+bst_tree=BSTree(lst)
+bst_tree.middle_Trasverse(bst_tree.root)
+print('')
+bst_tree.delete(4)
+bst_tree.middle_Trasverse(bst_tree.root)
+
+print('')
+bst_tree.delete(0)
+bst_tree.middle_Trasverse(bst_tree.root)
+
+print('')
+bst_tree.delete(9)
+bst_tree.middle_Trasverse(bst_tree.root)
